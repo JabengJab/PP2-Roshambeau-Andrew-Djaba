e3def351857502d138448d4f83c83aaf22fe29d6
Andrew Djaba <a.djaba@hotmail.com>
2024-06-21 11:07:55 +0000

    Modify scrips.js logic for determining the winner

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,3 @@
-// Rock Paper Scissors Spock
-
 const choices = ["rock", "paper", "scissors", "lizard", "spock"];
 const playerDisplay = document.getElementById("playerDisplay");
 const computerDisplay = document.getElementById("computerDisplay");
@@ -9,30 +7,28 @@ const computerScoreDisplay = document.getElementById("computerScoreDisplay");
 let playerScore = 0;
 let computerScore = 0;
 
-function playGame(playerChoice){
-
-    const computerChoice = choices[Math.floor(Math.random() *5)];
+function playGame(playerChoice) {
+    const computerChoice = choices[Math.floor(Math.random() * 5)];
     let result = "";
 
-    if(playerChoice === computerChoice){
+    if (playerChoice === computerChoice) {
         result = "IT'S A TIE";
-    }
-    else{
-        switch(playerChoice){
+    } else {
+        switch (playerChoice) {
             case "rock":
-                result = (computerChoice === "scissors", "lizard") ? "YOU WIN!" : "YOU LOSE!";
+                result = (computerChoice === "scissors" || computerChoice === "lizard") ? "YOU WIN!" : "YOU LOSE!";
                 break;
-                case "paper":
-                result = (computerChoice === "rock", "spock") ? "YOU WIN!" : "YOU LOSE!";
+            case "paper":
+                result = (computerChoice === "rock" || computerChoice === "spock") ? "YOU WIN!" : "YOU LOSE!";
                 break;
-                case "scissors":
-                result = (computerChoice === "paper", "lizard") ? "YOU WIN!" : "YOU LOSE!";
+            case "scissors":
+                result = (computerChoice === "paper" || computerChoice === "lizard") ? "YOU WIN!" : "YOU LOSE!";
                 break;
-                case "lizard":
-                result = (computerChoice === "spock", "paper") ? "YOU WIN!" : "YOU LOSE!";
+            case "lizard":
+                result = (computerChoice === "spock" || computerChoice === "paper") ? "YOU WIN!" : "YOU LOSE!";
                 break;
-                case "spock":
-                result = (computerChoice === "rock", "scissors") ? "YOU WIN!" : "YOU LOSE!";
+            case "spock":
+                result = (computerChoice === "rock" || computerChoice === "scissors") ? "YOU WIN!" : "YOU LOSE!";
                 break;
         }
     }
@@ -43,18 +39,16 @@ function playGame(playerChoice){
 
     resultDisplay.classList.remove("whiteText", "redText");
 
-    switch(result){
-        case "YOU WIN!": 
-                resultDisplay.classList.add("whiteText");
-                playerScore++;
-                playerScoreDisplay.textContent = playerScore;
-                break;
-        case "YOU LOSE!": 
-                resultDisplay.classList.add("redText");
-                computerScore++;
-                computerScoreDisplay.textContent = computerScore;
-                break;        
-
+    switch (result) {
+        case "YOU WIN!":
+            resultDisplay.classList.add("whiteText");
+            playerScore++;
+            playerScoreDisplay.textContent = playerScore;
+            break;
+        case "YOU LOSE!":
+            resultDisplay.classList.add("redText");
+            computerScore++;
+            computerScoreDisplay.textContent = computerScore;
+            break;
     }
-
 }
